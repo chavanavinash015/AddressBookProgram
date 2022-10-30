6e86ebb609da61c9b4078cb4db3c6f8168307107
arc-015 <chavanavinash015@gmail.com>
2022-10-30 19:10:40 +0530

    address book main will be separated

--- com/bridgelabz/addressbook/AddressBook.java
+++ com/bridgelabz/addressbook/AddressBook.java
@@ -5,13 +5,6 @@ import java.util.Scanner;
 public class AddressBook {
     static Scanner sc = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Address Book program");
-
-        AddressBook ad = new AddressBook();
-        ad.addContact();
-    }
-
     public void addContact() {
 
 //        String First_Name;
